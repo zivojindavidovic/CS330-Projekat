439635780c6fd3151919d90678288cbe98ffb985
zivojindavidovic <zivojin.davidovic2001@gmail.com>
2023-05-23 22:29:53 +0200

    changed theme

--- app/src/main/java/rs/ac/metropolitan/cs330_pz/screens/HomeScreen.kt
+++ app/src/main/java/rs/ac/metropolitan/cs330_pz/screens/HomeScreen.kt
@@ -7,14 +7,18 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import rs.ac.metropolitan.cs330_pz.AppViewModel
 import rs.ac.metropolitan.cs330_pz.screens.dialogs.RequestInternetPermissionsDialog
 
@@ -52,26 +56,34 @@ fun DestinationsList(vm: AppViewModel){
 
 @Composable
 fun SingleDestination(){
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
     ) {
-        Column {
-            Text(text = "Beograd - Novi Sad")
-        }
-        Column {
-            Text(text = "10 Km")
-        }
-        Spacer(
-            modifier = Modifier.
-                    weight(1f)
-        )
-        Icon(
-            Icons.Default.ArrowForward,
-            contentDescription = "View Destination",
-            modifier = Modifier.clickable {
+       Row(
+           verticalAlignment = Alignment.CenterVertically,
+           modifier = Modifier
+               .padding(8.dp)
+       ) {
+           Column {
+               Text(text = "Beograd - Novi Sad")
+           }
+           Column {
+               Text(text = "10 Km")
+           }
+           Spacer(
+               modifier = Modifier.
+               weight(1f)
+           )
+           Icon(
+               Icons.Default.ArrowForward,
+               contentDescription = "View Destination",
+               modifier = Modifier.clickable {
 
-            }
-        )
+               }
+           )
+       }
     }
 }
 
